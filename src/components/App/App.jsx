583277aa48eb90f9ce525ed44f6d468fb9091d49
Alex Flores <alexdev-ind@outlook.com>
2021-09-20 19:05:41 -0400

    some corrections

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ const App = () => {
         <div>
             <h1>Hey the application is alive !</h1>
             <h1>Welcome Martina Serranova !</h1>
-            <h1>I'm gonna see you in a couple of mount </h1>
+            <h1>I'm gonna see you in a couple of mounths </h1>
             <h1>Tell you at your parent that they have a debt wit me</h1>
             <h1>Hi Cote</h1>
         </div>
