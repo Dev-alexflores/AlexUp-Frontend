b025c29f3b8f6cc2d947d539008eeea29bf7ae96
Alex Flores <alexdev-ind@outlook.com>
2021-09-23 16:15:24 -0400

    nav-bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,18 @@
+import Navbar from './../NavBar/Navbar';
+import { HashRouter as Router } from "react-router-dom";
+
 const App = () => {
     return (
+        <Router>
         <div>
+            <Navbar/>
             <h1>Hey the application is alive !</h1>
             <h1>Welcome Martina Serranov !</h1>
             <h1>I'm gonna see you in a couple of mounths </h1>
             <h1>Tell you at your parent that they have a debt wit me</h1>
             <h1>Hi Cote</h1>
         </div>
+        </Router>
     );
 }
 
